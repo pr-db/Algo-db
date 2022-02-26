3ce26bf94ac5e59d8c5ca393fa6d75d9a6a1a6e3
pr-db <prakhar.dub@gmail.com>
2022-02-26 10:45:20 +0530

    A3 p015

--- A3/p014_unstableSpaceships.cpp
+++ A3/p014_unstableSpaceships.cpp
@@ -9,15 +9,36 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	ll n;
-	cin >> n ;
+	cin >> n;
 	v a(n);
 	for (ll i = 0; i < n; i++)
 		cin >> a[i];
-	sort(a.begin(),a.end());
-	v dif(n-1);
-	for (int i = 0; i < n-1;i++)
-	{
+	sort(a.begin(), a.end());
+	v dif(n - 1);
+	for (int i = 0; i < n - 1; i++)
 		dif[i] = a[i + 1] - a[i];
+	ll fmax = *max_element(dif.begin(), dif.end()), smax = 0, fc = 0;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (dif[i] > smax && dif[i] != fmax)
+			smax = dif[i];
+		else if (dif[i] == fmax)
+			fc++;
 	}
-		return 0;
+	if (fc > 1)
+		smax = fmax;
+	v nb(4);
+	for (int i = 0; i < n - 1; i++)
+	{
+		if(dif[i]==fmax)
+		{
+		if(i>0)
+			nb[0] = dif[i - 1];
+		else
+		{
+			/* code */
+		}
+		
+	}
+	return 0;
 }
---
+++ A3/p015_cowArrival.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n,ans=0;
+	cin >> n;
+	vector<pair<int, int>> c;
+	for (int i = 0; i < n;i++)
+	{
+		ll x, y;
+		cin >> x >> y;
+		c.push_back(make_pair(x,y));
+	}
+	sort(c.begin(), c.end());
+	ans += c[0].first+c[0].second;
+	
+	for (int i = 1; i < n; i++)
+	{
+		if(ans<c[i].first)
+			ans += (c[i].first-ans);
+		ans += c[i].second;
+	}
+	cout << ans;
+	return 0;
+}
