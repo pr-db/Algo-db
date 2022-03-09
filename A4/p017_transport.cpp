6276acdef22182e22cb9535f51e8244efcca5fa7
pr-db <prakhar.dub@gmail.com>
2022-03-09 21:54:26 +0530

    A4 p017

--- A4/p017_transport.cpp
+++ A4/p017_transport.cpp
@@ -1,34 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-
+ll n, m, c;
+bool predicate(vector<ll> &a, ll M)
+{
+	bool ans = true;
+	ll c1 = c, m1 = m, n1 = 0;
+	while (n1 < n && m1 > 0)
+	{
+		c1 = c;
+		ll n2 = n1;
+		n1++;
+		while (c1 > 1 && n1 < n)
+		{
+			n1++;
+			c1--;
+			if ((a[n1 - 1] - a[n2]) > M)
+			{
+				n1--;
+				break;
+			}
+		}
+		m1--;
+	}
+	if (n1 != n)
+		ans = false;
+	return ans;
+}
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	ll n, m, c;
 	cin >> n >> m >> c;
-	vector<ll> a(n);
-	vector<vector<ll>> b;
-
+	vector<ll> a;
 	for (ll i = 0; i < n; i++)
-		cin >> a[i];
+	{
+		ll temp;
+		cin >> temp;
+		a.push_back(temp);
+	}
 	sort(a.begin(), a.end());
-	ll ans = 0, f = 0;
-	for (ll i = 0; i < m && f < n; i++)
+	ll ans = a[n - 1] - a[0];
+	ll L = 0, R = ans + 1, M;
+
+	while (L + 1 < R)
 	{
-		vector<ll> temp;
-		for (ll j = 0; j < c && f < n; j++, f++)
-			temp.push_back(a[i * c + j]);
-		b.push_back(temp);
+		M = L + (R - L) / 2;
+		if (predicate(a, M))
+			R = M;
+		else
+			L = M;
 	}
-	f = 0;
-	for (ll i = 0; i < b.size(); i++)
-	{
-		for (ll j = 0; j < b[i].size(); j++)
-			cout << b[i][j] << " ";
-		cout << "\n";
-	}
-	cout << ans;
+	cout << "\n";
+	cout << L + 1;
 	return 0;
 }
