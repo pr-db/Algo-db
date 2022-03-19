d9c7b472895e92b492ac5fbc5170e2f27b4318b6
pr-db <prakhar.dub@gmail.com>
2022-03-19 18:31:27 +0530

    format changes

--- format/format.cpp
+++ format/format.cpp
@@ -1,23 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-int func(int a[], int n)
-{
-	int ans;
-	for (int i = 0; i < n; i++)
-	{
-	}
-	return ans;
-}
+#define FastIO                        \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);                    \
+	cout.tie(NULL)
 int main()
 {
-	// ios_base::sync_with_stdio(false);
-	// cin.tie(NULL);
-	int n, k;
-	int a[] = {2, 6, 3, 4, 7, 2, 10, 3, 2, 1};
-	n = sizeof(a) / sizeof(a[0]);
-	k = 5;
-	int ans = func(a, n);
-	cout << ans;
+	FastIO;
+	ll n, k;
+	cin >> n >> k;
+	vector<ll> a(n);
+	for (ll i = 0; i < n; i++)
+		cin >> a[i];
 	return 0;
 }
--- format/format2.cpp
+++ format/format2.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-#define FastIO                        \
-	ios_base::sync_with_stdio(false); \
-	cin.tie(NULL);                    \
-	cout.tie(NULL)
+int func(int a[], int n)
+{
+	int ans;
+	for (int i = 0; i < n; i++)
+	{
+	}
+	return ans;
+}
 int main()
 {
-	FastIO;
-	ll n, k;
-	cin >> n >> k;
-	vector<ll> a(n);
-	for (ll i = 0; i < n; i++)
-		cin >> a[i];
+	// ios_base::sync_with_stdio(false);
+	// cin.tie(NULL);
+	int n, k;
+	int a[] = {2, 6, 3, 4, 7, 2, 10, 3, 2, 1};
+	n = sizeof(a) / sizeof(a[0]);
+	k = 5;
+	int ans = func(a, n);
+	cout << ans;
 	return 0;
 }
