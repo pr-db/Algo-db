d84a3d5aa464f8b0eda987e004400c958d9b38cf
pr-db <learn.pd9913@gmail.com>
2022-06-17 12:18:39 +0530

    disco jumps

--- A19_DijkstraAndMST/x02_discoJumps.cpp
+++ A19_DijkstraAndMST/x02_discoJumps.cpp
@@ -5,6 +5,7 @@ typedef long long int ll;
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL);                    \
 	cout.tie(NULL)
+
 enum
 {
 	T,
@@ -12,79 +13,128 @@ enum
 	B,
 	L
 };
-class Edge
+class Graph
 {
 public:
-	long long u, v, w;
-	bool operator<(Edge const &other)
-	{ // helps find edge with smaller wegiht b/w two variable of Edge type
-		return w < other.w;
+	ll n;
+	vector<vector<pair<ll, ll>>> adj;
+	vector<vector<char>> disks;
+
+	Graph(ll _n)
+	{
+		this->n = _n;
+		this->adj = vector<vector<pair<ll, ll>>>(_n);
+	}
+
+	void addEdge(ll u, ll v, ll wt)
+	{
+		adj[u].push_back(make_pair(v, wt));
 	}
 };
 
-class DSU
+void dijkstra(Graph &g, ll s, vector<ll> &d)
 {
-public:
-	vector<ll> par;
-	DSU(ll nComponents)
-	{
-		par.resize(nComponents, -1);
-	}
-	ll root(ll v)
-	{
-		if (par[v] < 0)
-			return v;
-		return par[v] = root(par[v]);
-	} // finds component number of an entity
+	ll n = g.adj.size();
+	d.assign(n, INT_MAX);
 
-	void merge(ll x, ll y)
-	{ // merges two different components into one
-		if ((x = root(x)) == (y = root(y)))
-			return;
-		if (par[y] < par[x])
-			swap(x, y);
-		par[x] += par[y];
-		par[y] = x;
-	}
-};
-
-void kruskal(ll n, vector<Edge> edges)
-{
-	long long ans = 0;
-	DSU dsu(n + 1);
-
-	sort(edges.begin(), edges.end());
-	for (ll i = 0; i < edges.size(); ++i)
+	d[s] = 0;
+	using pii = pair<ll, ll>;
+	priority_queue<pii, vector<pii>, greater<pii>> q;
+	q.push({0, s});
+	while (!q.empty())
 	{
-		if (dsu.root(edges[i].u) != dsu.root(edges[i].v))
-		{ // edge connects two nodes of different components
-			dsu.merge(edges[i].u, edges[i].v);
-			ans += edges[i].w;
+		ll v = q.top().second;
+		ll d_v = q.top().first;
+		q.pop();
+		if (d_v != d[v])
+			continue;
+
+		for (auto edge : g.adj[v])
+		{
+			ll to = edge.first;
+			ll len = edge.second;
+
+			if (d[v] + len < d[to])
+			{
+				d[to] = d[v] + len;
+				q.push({d[to], to});
+			}
 		}
-		cout << dsu.par[i] << " ";
 	}
-
-	unordered_set<ll> countComponents;
-	for (ll i = 1; i <= n; ++i)
-		countComponents.insert(dsu.root(i));
-	if (countComponents.size() > 1)
-		cout << "IMPOSSIBLE\n";
-	else
-		cout << ans << "\n";
 }
 
 int main()
 {
 	FastIO;
+
 	ll n, m;
 	cin >> n >> m;
-	vector<Edge> edges;
-	vector<vector<ll>> disks;
-	for (ll i = 0; i < m * n; i++)
+	Graph g(n * m);
+
+	for (ll i = 0; i < n; i++)
 	{
-		vector<ll> disk(4);
+		for (ll j = 0; j < m; j++)
+		{
+			vector<char> disk(4);
+			cin >> disk[T] >> disk[R] >> disk[B] >> disk[L];
+			g.disks.push_back(disk);
+		}
 	}
-	kruskal(n, edges);
+	ll s = g.disks.size();
+
+	for (ll i = 0; i < n; i++)
+	{
+		for (ll j = 0; j < m; j++)
+		{
+			ll ind = i * m + j;
+			ll k = 0;
+			if (i > 0)
+			{
+				k = 0;
+				while (g.disks[ind][k] != g.disks[ind - m][B])
+					k++;
+				if (T - k >= 0)
+					g.addEdge(ind, ind - m, T - k);
+				else
+					g.addEdge(ind, ind - m, T - k + 4);
+			}
+			if (j < m - 1)
+			{
+				k = 0;
+				while (g.disks[ind][k] != g.disks[ind + 1][L])
+					k++;
+
+				if (R - k >= 0)
+					g.addEdge(ind, ind + 1, R - k);
+				else
+					g.addEdge(ind, ind + 1, R - k + 4);
+			}
+			if (j > 0)
+			{
+				k = 0;
+				while (g.disks[ind][k] != g.disks[ind - 1][R])
+					k++;
+				if (L - k >= 0)
+					g.addEdge(ind, ind - 1, L - k);
+				else
+					g.addEdge(ind, ind - 1, L - k + 4);
+			}
+			if (i < n - 1)
+			{
+				k = 0;
+				while (g.disks[ind][k] != g.disks[ind + m][T])
+					k++;
+				if (B - k >= 0)
+					g.addEdge(ind, ind + m, B - k);
+				else
+					g.addEdge(ind, ind + m, B - k + 4);
+			}
+		}
+	}
+
+	vector<ll> d;
+	dijkstra(g, 0, d);
+	cout << d[m*n - 1];
 
 	return 0;
 }
