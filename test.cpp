44d0b9bf11948bdd4a878b8d4c098c63b76885cd
pr-db <prakhar.dub@gmail.com>
2022-07-21 08:24:51 +0530

    DP 01/01

--- test.cpp
+++ test.cpp
@@ -9,9 +9,24 @@ int main()
 {
 	FastIO;
 	ll n, k;
-	cin >> n;
+	cin >> n ;
 	vector<ll> a(n);
 	for (ll i = 0; i < n; i++)
 		cin >> a[i];
+
+	ll s = 0,e = n;
+	while (s+1 < e)
+	{
+		int mid = (s + e) / 2;
+		if (a[mid] == -1)
+		{
+			e = mid ;
+		}
+		else
+		{
+			s = mid;
+		}
+	}
+	cout << e;
 	return 0;
 }
