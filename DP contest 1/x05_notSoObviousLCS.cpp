0cd9021293d44c309ae2994a142cbfa08676c8c8
pr-db <prakhar.dub@gmail.com>
2022-07-31 14:37:14 +0530

    not so obvious lcs

--- DP contest 1/x05_notSoObviousLCS.cpp
+++ DP contest 1/x05_notSoObviousLCS.cpp
@@ -5,38 +5,47 @@ typedef long long int ll;
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL);                    \
 	cout.tie(NULL)
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long int ll;
-#define FastIO                        \
-	ios_base::sync_with_stdio(false); \
-	cin.tie(NULL);                    \
-	cout.tie(NULL)
 
-vector<vector<ll>> dp;
 vector<ll> a;
-ll n;
-ll lis(ll i, ll j)
+vector<ll> b;
+vector<ll> c;
+int lis()
 {
-	if (i == n)
-		return 0;
-	if (dp[i][j] >= 0)
-		return dp[i][j];
-
-	if (a[i] > a[j])
-		return dp[i][j] = max(dp[i][j], 1 + lis(i + 1, i));
-	return dp[i][j] = max(dp[i][j], lis(i + 1, j));
+	int n = c.size();
+	int ans = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (c[i] > c[ans])
+			c[++ans] = c[i];
+		else
+			*lower_bound(begin(c), begin(c) + ans, c[i]) = c[i];
+		// cout << ans << " ";
+	}
+	if (c.size())
+		return ans + 1;
+	return 0;
 }
 int main()
 {
 	FastIO;
-	cin >> n;
+	int n, m;
+	cin >> n >> m;
+	b = vector<ll>(m);
 	a = vector<ll>(n);
-	dp = vector<vector<ll>>(n, vector<ll>(n, -1));
+	map<ll, ll> mp;
 	for (ll i = 0; i < n; i++)
+	{
 		cin >> a[i];
+		mp[a[i]] = i + 1;
+	}
+	for (ll i = 0; i < m; i++)
+	{
+		cin >> b[i];
+		if (mp[b[i]])
+			c.push_back(mp[b[i]]);
+	}
 
-	cout << lis(0, 0);
+	cout << m - lis();
 
 	return 0;
 }
